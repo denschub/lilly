10c9342fcd5701114dc7da3d137b201c50d69789
Dennis Schubert <software@dsx.cc>
2014-05-27 02:20:35 +0200

    Introduce a little bit of project structure...

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,37 +1,3 @@
-function setLoadingSpinner() {
-  document.getElementById("loader").classList.add("fa-spin");
-}
-
-function unsetLoadingSpinner() {
-  document.getElementById("loader").classList.remove("fa-spin");
-}
-
-function dropCancelHandler(event) {
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'copy';
-  return false;
-}
-
-function flash(color) {
-  document.body.classList.add("flash-"+color);
-  window.setTimeout(function() {
-    document.body.classList.remove("flash-"+color);
-  }, 2100);
-}
-
-function dropHandler(category, event) {
-  event.preventDefault();
-  setLoadingSpinner();
-  var url = event.dataTransfer.getData('Text');
-  if(validUrl(url)) {
-    flash("green");
-    unsetLoadingSpinner();
-  } else {
-    flash("red");
-    unsetLoadingSpinner();
-  }
-}
-
 function validUrl(url) {
   /**
    * this regex is extracted from jquery-validation by Jörn Zaefferer
@@ -41,11 +7,10 @@ function validUrl(url) {
   return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)*(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
 }
 
+window.app = {
+  ui: new Ui()
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-  var dropzones = document.querySelectorAll(".dropzone");
-  for(var i=0; i < dropzones.length; i++) {
-    dropzones[i].addEventListener("dragenter", dropCancelHandler);
-    dropzones[i].addEventListener("dragover", dropCancelHandler);
-    dropzones[i].addEventListener("drop", dropHandler.bind(this, dropzones[i].id));
-  }
+  app.ui.bindEvents();
 });
---
+++ public/assets/js/ui.js
@@ -0,0 +1,48 @@
+var Ui = (function() {
+  function Ui() {}
+
+  Ui.prototype.bindEvents = function() {
+    var dropzones = document.querySelectorAll(".dropzone");
+    for(var i=0; i < dropzones.length; i++) {
+      dropzones[i].addEventListener("dragenter", this.dropCancelHandler);
+      dropzones[i].addEventListener("dragover", this.dropCancelHandler);
+      dropzones[i].addEventListener("drop", this.dropHandler.bind(this, dropzones[i].id));
+    }
+  }
+
+  Ui.prototype.startLoadingSpinner = function() {
+    document.getElementById("loader").classList.add("fa-spin");
+  };
+
+  Ui.prototype.stopLoadingSpinner = function() {
+    document.getElementById("loader").classList.remove("fa-spin");
+  };
+
+  Ui.prototype.flash = function(color) {
+    document.body.classList.add("flash-"+color);
+    window.setTimeout(function() {
+      document.body.classList.remove("flash-"+color);
+    }, 2100);
+  }
+
+  Ui.prototype.dropCancelHandler = function(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+    return false;
+  }
+
+  Ui.prototype.dropHandler = function(category, event) {
+    event.preventDefault();
+    this.startLoadingSpinner();
+    var url = event.dataTransfer.getData('Text');
+    if(validUrl(url)) {
+      this.flash("green");
+      this.stopLoadingSpinner();
+    } else {
+      this.flash("red");
+      this.stopLoadingSpinner();
+    }
+  }
+
+  return Ui;
+})();
